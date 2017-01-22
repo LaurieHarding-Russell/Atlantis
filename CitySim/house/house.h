3ed1b2d1d1c7073a706e727b39ac63d56f53f759
LaurieHarding-Russell <lauriehardingrussell@gmail.com>
2017-01-21 22:25:24 -0600

    Creating a basic project.

---
+++ CitySim/house/house.h
@@ -0,0 +1,17 @@
+#ifndef house_h
+#define house_h
+
+#include "./household/household.h"
+
+class House {
+
+public:
+  
+
+private:
+  int value;
+  bool power;
+  bool abandoned;
+}
+
+#endif
---
+++ CitySim/house/household/household.h
@@ -0,0 +1,11 @@
+#ifndef household_h
+#define household_h
+
+class House {
+public:
+
+private:
+
+}
+
+#endif
